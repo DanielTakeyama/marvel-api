0c7563fd85974c0effcf8030f022ea738e9fa931
DanielTakeyama <daniel.martins.a.prado@gmail.com>
2025-01-10 16:20:44 -0300

    Molde para buscar a API da marvel feita, agora vou começar a configurar com a API da marvel

--- services/buscarTodosPersonagens.tsx
+++ services/buscarTodosPersonagens.tsx
@@ -1,26 +1,45 @@
-import { useEffect, useState} from 'react';
+import { useState, useEffect } from 'react';
+import MoldePersonagem from '@/components/moldePersonagem';
+
+
+interface Personagem{
+    id: string;
+    name: string;
+    image: string;
+    species: string;
+}
+
+
 
 export default function BuscarTodosPersonagens(){
-    const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
-
+    const [dados, setDados] = useState<Personagem[]>([])
+    
     useEffect(()=>{
-        const buscarDados = async()=>{
+
+        const buscarDados = async () => {
             try{
-                const dados = await fetch("https://rickandmortyapi.com/api/character/539");
-                if(!dados.ok){
-                    throw new Error(`Falha ao fazer a requisição para o servidor do rick and morty, erro: ${dados.status} , ${dados.statusText}`);
+                const dadosApi = await fetch("https://rickandmortyapi.com/api/character");
+                if(!dadosApi.ok){
+                    throw new Error (`Falha ao fazer a requisição para o servidor do Rick And Morty, status: ${dadosApi.statusText}`);
                 }
-                const dadosJson = await dados.json();
-                setData(dadosJson);
-            } 
-            catch(err){
-                setError(null);
+                const dadosJson = await dadosApi.json();
+                setDados(dadosJson.results);
+
+            } catch(err) {
+                console.log(err)
             }
         };
+
         buscarDados();
-    },[]);
-    return (<>
-        <h1>{JSON.stringify(data)}</h1>
-    </>)
+    }, []);
+
+    console.log(dados);
+    return(
+    <>
+        {dados.length === 0 ? <p>Carregando...</p> : dados.map((personagem)=>(
+        <MoldePersonagem key={personagem.id} src={personagem.image} alt={personagem.name} id={personagem.id} name={personagem.name} desc={personagem.species}/>
+        ))}
+    </>
+);
+
 }
